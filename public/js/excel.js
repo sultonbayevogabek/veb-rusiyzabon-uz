7983446e58d4cb47c92ef35d818b5db1cc88190a
sultonbayevogabek <sultonbayevogabek@gmail.com>
2024-10-17 18:40:36 +0500

    initial commit

--- public/js/excel.js
+++ public/js/excel.js
@@ -2,12 +2,16 @@
 
 let user = JSON.parse(localStorage.getItem('user'));
 
+document.querySelector('#joinToChannel')?.addEventListener('click', () => {
+  window.location.href = 'https://kun.uz';
+})
+
 if (user && user?.name && user?.phone && user?.time) {
     const formData = new FormData();
     formData.append('Ismi', user?.name);
     formData.append('Telefon raqami', user?.phone);
     formData.append(`Ro'yxatdan o'tgan vaqti`, user?.time);
-    fetch('https://script.google.com/macros/s/AKfycbySTvtjyn6lU-zWpXdcpI5onCjo5FurWpIx-GPik5TBbnqiLmDPj1fkhIhqOPZcEnycow/exec', {
+    fetch('https://script.google.com/macros/s/AKfycbyw9egR7-12cZMmgZHQmEtfAxKo6pJ8ISrR7D-MHOuXMsomPOxHSjYRe92THBNAwKRE/exec', {
         method: 'POST',
         body: formData
     })
