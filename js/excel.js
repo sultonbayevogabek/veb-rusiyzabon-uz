60284d95506b18dd3f7dbb025d526e86fc101400
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-03-24 15:11:36 +0500

    initial commit

--- js/excel.js
+++ js/excel.js
@@ -3,7 +3,7 @@
 let user = JSON.parse(localStorage.getItem('user'));
 
 document.querySelector('#joinToChannel')?.addEventListener('click', () => {
-  window.location.href = 'https://t.me/+rneOxYu4aDYzNDYy';
+  window.location.href = 'https://t.me/+Gf6k4gOJ1xlkNWMy';
 })
 
 if (user && user?.name && user?.phone && user?.time) {
